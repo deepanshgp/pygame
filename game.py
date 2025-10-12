a4825c258764422cfb59df0640c2f21e22005ace
Deepansh Gupta <deepanshgp@gmail.com>
2025-10-12 22:52:22 +0530

    tile generation and rendering.

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
-# Example file showing a circle moving on self.screen
 import pygame
+
+from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
@@ -12,14 +14,23 @@ class Game:
 
         self.movement = [False, False]
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
+        }
+        self.tilemap = Tilemap(self, tile_size=16)
         
     def run(self):
         while True:
             # fill the self.screen with a color to wipe away anything from last frame
             self.screen.fill("purple")
             
-            self.player.update(self.display, (self.movement[1] - self.movement[0], 0))
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -40,7 +51,7 @@ class Game:
 
                 self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
                 pygame.display.update()
-                self.clock.tick(144) # limit to 60 FPS
+                self.clock.tick(60) # limit to 60 FPS
 
 if __name__ == "__main__":
     game = Game()
--- scripts/entities.py
+++ scripts/entities.py
@@ -12,33 +12,34 @@ class PhysicsEntity:
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
         
-    def update(self, display, movement=(0, 0)):
+    def update(self, tilemap, movement=(0, 0)):
         self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
         
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
         
         self.pos[0] += frame_movement[0]
         entity_rect = self.rect()
-        rect = pygame.Rect(0, 200, 320, 40)
-        if entity_rect.colliderect(rect):
-            if frame_movement[0] > 0:
-                entity_rect.right = rect.left
-                self.collisions['right'] = True
-            if frame_movement[0] < 0:
-                entity_rect.left = rect.right
-                self.collisions['left'] = True
-            self.pos[0] = entity_rect.x
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
         
         self.pos[1] += frame_movement[1]
         entity_rect = self.rect()
-        if entity_rect.colliderect(rect):
-            if frame_movement[1] > 0:
-                entity_rect.bottom = rect.top
-                self.collisions['down'] = True
-            if frame_movement[1] < 0:
-                entity_rect.top = rect.bottom
-                self.collisions['up'] = True
-            self.pos[1] = entity_rect.y
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
         
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
         
@@ -46,4 +47,4 @@ class PhysicsEntity:
             self.velocity[1] = 0
         
     def render(self, surf):
-        surf.blit(surf, self.pos)
+        surf.blit(self.game.assets['player'], self.pos)       
