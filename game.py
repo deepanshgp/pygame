61b8257df4d782362f939f66509ad7c196204031
Deepansh Gupta <deepanshgp@gmail.com>
2025-10-16 00:52:13 +0530

    rendering fixes

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from scripts.tilemap import Tilemap
 class Game:
     def __init__(self):
         # pygame setup
+        pygame.init()
         pygame.display.set_caption('game')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
@@ -25,8 +26,8 @@ class Game:
         
     def run(self):
         while True:
-            # fill the self.screen with a color to wipe away anything from last frame
-            self.screen.fill("purple")
+            self.display.fill((14, 219, 248))
+            self.tilemap.render(self.display)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
@@ -48,10 +49,10 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-
-                self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
-                pygame.display.update()
-                self.clock.tick(60) # limit to 60 FPS
+            
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
 
 if __name__ == "__main__":
     game = Game()
