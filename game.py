1982a517163f7d9c411e5ed2d1cfd9fb6a16b4d6
Arnav Patil <arnav24beit@student.mes.ac.in>
2025-10-17 01:21:35 +0530

    Added Camera and Sky bg

--- game.py
+++ game.py
@@ -20,17 +20,26 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png')
         }
         self.tilemap = Tilemap(self, tile_size=16)
+        self.scroll = [0, 0]
         
     def run(self):
         while True:
-            self.display.fill((14, 219, 248))
-            self.tilemap.render(self.display)
+            self.display.blit(self.assets['background'], (0,0))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 15
+
+            self.scroll[1] += (self.player.rect().centery - self.display.get_width() / 2 - self.scroll[1]) / 7
+
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset = render_scroll)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset = render_scroll)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
